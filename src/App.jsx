5023a5692a0ac4243191b3609a0560cd62fe0458
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-11-21 15:05:59 -0800

    account sign in initializes, user credentials recieved and info populates view component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null);
+
+  function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID Token: ", response.credential);
+    let userObject = jwt_decode(response.credential);
+    console.log("User Object: ", userObject);
+    setUser(userObject);
+    document.getElementById("signInContainer").hidden = true;
+  }
+
+  useEffect(() => {
+    console.log(import.meta.env.VITE_GOOGLE_CLIENT_ID);
+    // google object comes from script in index.html
+    google.accounts.id.initialize({
+      // Referrer-Policy: no-referrer-when-downgrade, 
+      client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+      // when someone logs in, this is the function we call
+      callback: handleCallbackResponse,
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInContainer"),
+      { theme: "outline", size: "large" }
+    )
+  }, []);
 
   return (
     <div className="App">
-      <div className='loginComponent'>
-
-      </div>
+      <div id="signInContainer"></div>
+      {user && (
+        <div>
+          <img src={user.picture}/>
+          <h3>{user.name}</h3>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
