1647f8e3cca3e77d77d8098f8e3d4eb391c3eac3
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-11-21 09:12:55 -0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div className='loginComponent'>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
